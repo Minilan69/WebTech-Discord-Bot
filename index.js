9317ed54730386ae66f6e698ac3af122aaa9e2a1
Minilan <minilancraft@gmail.com>
2024-12-12 19:41:03 +0100

    Change image name Add date in logging message

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Import
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { Client, Events, GatewayIntentBits, time, TimestampStyles } = require("discord.js");
 const { token } = require("./config.json");
 
 // New instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.once(Events.ClientReady, (readyClient) => {
-  console.log(`Connected as ${readyClient.user.tag}`);
+  console.log(
+    `Connected as ${readyClient.user.username} at ${Date()}`
+  );
 });
 
 // Launch Bot
