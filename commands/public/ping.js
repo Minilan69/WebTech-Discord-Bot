cd87eb81d4c796b1e750b0083c894b222beedb24
Minilan <minilancraft@gmail.com>
2024-12-17 02:31:19 +0100

    Ping Big Review

--- commands/public/ping.js
+++ commands/public/ping.js
@@ -1,9 +1,7 @@
-// Imports
 const { SlashCommandBuilder } = require("discord.js");
 const os = require("os");
 const process = require("process");
 
-// Command
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
@@ -14,30 +12,27 @@ module.exports = {
     await interaction.deferReply();
 
     try {
+      // Ping du bot et de l'API Discord
+      const sent = await interaction.followUp({
+        content: "Calcul en cours...",
+        fetchReply: true,
+      });
       const botLatency = sent.createdTimestamp - interaction.createdTimestamp;
-      const apiLatency = Math.round(interaction.client.ws.ping);
-
+      let apiLatency = Math.round(interaction.client.ws.ping);
+      if (apiLatency == -1) {
+        apiLatency = 0;
+      }
       const botUptime = formatUptime(process.uptime());
 
-      const cpuUsage = process.cpuUsage();
-      const memoryUsage = process.memoryUsage();
-
-      // 4. Message de réponse
+      // Message final
       await interaction.editReply(
-        `🏓 **Pong ! Voici les informations :**\n
-        🔹 **Ping du bot :** \`${botLatency}ms\`\n
-        🔹 **Latence API Discord :** \`${apiLatency}ms\`\n
-        🕒 **Uptime du bot :** \`${botUptime}\`\n
-        ⚙️ **CPU :** \`${cpuUsage.user / 1000} ms (User)\` | \`${
-          cpuUsage.system / 1000
-        } ms (System)\`
+        `
+        🔹 **Ping du bot :** ${botLatency}ms\n🔹 **Latence API Discord :** ${apiLatency}ms\n🔹 **Uptime du bot :** ${botUptime}
         `
       );
     } catch (error) {
       console.error("[❌ERROR]", error);
-      await interaction.editReply(
-        "❌ Impossible de ping"
-      );
+      await interaction.editReply("❌ Impossible de ping");
     }
   },
 };
