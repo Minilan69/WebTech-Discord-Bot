5549664dacdb71c05351352a34eefcfb615f44dc
Minilan <minilancraft@gmail.com>
2024-12-20 00:17:48 +0100

    Make Condition Dor Ban & Kick

--- commands/admins/ban.js
+++ commands/admins/ban.js
@@ -17,6 +17,31 @@ module.exports = {
     await interaction.deferReply();
     const user = interaction.options.getUser("member");
 
+    // Verify if the caller has the permission to ban members
+    if (!interaction.guild.members.me.permissions.has("BAN_MEMBERS")) {
+      return interaction.reply({
+        content: "❌ Tu n'as pas la permission de bannir des membres",
+        ephemeral: true,
+      });
+    }
+
+    // Verify is not an admin
+    const memberToBan = await interaction.guild.members.fetch(user.id);
+    if (memberToBan.permissions.has("ADMINISTRATOR")) {
+      return interaction.reply({
+        content: "❌ Vous ne pouvez pas bannir un administrateur",
+        ephemeral: true,
+      });
+    }
+
+    // Verify if the user is not the caller
+    if (user.id === interaction.user.id) {
+      return interaction.reply({
+        content: "❌ Tu ne peux pas te bannir toi-même",
+        ephemeral: true,
+      });
+    }
+
     // Ban User
     try {
       guild.members.ban(user);
--- commands/admins/kick.js
+++ commands/admins/kick.js
@@ -17,15 +17,38 @@ module.exports = {
     await interaction.deferReply();
     const user = interaction.options.getUser("member");
 
+    // Verify if the caller has the permission to kick members
+    if (!interaction.guild.members.me.permissions.has("KICK_MEMBERS")) {
+      return interaction.reply({
+        content: "❌ Tu n'as pas la permission de Kick des membres",
+        ephemeral: true,
+      });
+    }
+
+    // Verify is not an admin
+    const memberToKick = await interaction.guild.members.fetch(user.id);
+    if (memberToKick.permissions.has("ADMINISTRATOR")) {
+      return interaction.reply({
+        content: "❌ Vous ne pouvez pas kick un administrateur",
+        ephemeral: true,
+      });
+    }
+
+    // Verify if the user is not the caller
+    if (user.id === interaction.user.id) {
+      return interaction.reply({
+        content: "❌ Tu ne peux pas te kick toi-même",
+        ephemeral: true,
+      });
+    }
+
     // Kick User
     try {
       guild.members.kick(user);
       await interaction.editReply(`✅ <@${user.id}> a été kick`);
     } catch (error) {
       console.error("[❌ERROR]", error);
-      await interaction.editReply(
-        "❌ Impossible de kick le membre"
-      );
+      await interaction.editReply("❌ Impossible de kick le membre");
     }
   },
 };
