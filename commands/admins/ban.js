a59cd46d4e1a0f5fc3b1a60e647f0ca10b2bca05
Minilan <minilancraft@gmail.com>
2024-12-20 00:48:24 +0100

    Ban & Kick Finish

--- commands/admins/ban.js
+++ commands/admins/ban.js
@@ -1,5 +1,5 @@
 // Imports
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, PermissionsBitField } = require("discord.js");
 
 // Command's Attributes
 module.exports = {
@@ -15,21 +15,22 @@ module.exports = {
   // Execution
   async execute(interaction) {
     await interaction.deferReply();
+    const caller = interaction.member;
     const user = interaction.options.getUser("member");
+    const member = await interaction.guild.members.fetch(user.id);
 
     // Verify if the caller has the permission to ban members
-    if (!interaction.guild.members.me.permissions.has("BAN_MEMBERS")) {
+    if (!caller.roles.cache.has("1315425516853133404")) {
       return interaction.reply({
-        content: "❌ Tu n'as pas la permission de bannir des membres",
+        content: "❌ Tu n'as pas la permission de ban des membres",
         ephemeral: true,
       });
     }
 
     // Verify is not an admin
-    const memberToBan = await interaction.guild.members.fetch(user.id);
-    if (memberToBan.permissions.has(8)) {
+    if (member.roles.cache.has("1315425516853133404")) {
       return interaction.reply({
-        content: "❌ Vous ne pouvez pas bannir un administrateur",
+        content: "❌ Vous ne pouvez pas ban un administrateur",
         ephemeral: true,
       });
     }
@@ -44,7 +45,7 @@ module.exports = {
 
     // Ban User
     try {
-      guild.members.ban(user);
+      interaction.guild.members.ban(user);
       await interaction.editReply(`✅ <@${user.id}> a été ban`);
     } catch (error) {
       console.error("[❌ERROR]", error);
--- commands/admins/kick.js
+++ commands/admins/kick.js
@@ -1,5 +1,5 @@
 // Imports
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, PermissionsBitField } = require("discord.js");
 
 // Command's Attributes
 module.exports = {
@@ -15,19 +15,20 @@ module.exports = {
   // Execution
   async execute(interaction) {
     await interaction.deferReply();
+    const caller = interaction.member;
     const user = interaction.options.getUser("member");
+    const member = await interaction.guild.members.fetch(user.id);
 
     // Verify if the caller has the permission to kick members
-    if (!interaction.guild.members.me.permissions.has("KICK_MEMBERS")) {
+    if (!caller.roles.cache.has("1315425516853133404")) {
       return interaction.reply({
-        content: "❌ Tu n'as pas la permission de Kick des membres",
+        content: "❌ Tu n'as pas la permission de kick des membres",
         ephemeral: true,
       });
     }
 
     // Verify is not an admin
-    const memberToKick = await interaction.guild.members.fetch(user.id);
-    if (memberToKick.permissions.has(8)) {
+    if (member.roles.cache.has("1315425516853133404")) {
       return interaction.reply({
         content: "❌ Vous ne pouvez pas kick un administrateur",
         ephemeral: true,
@@ -44,7 +45,7 @@ module.exports = {
 
     // Kick User
     try {
-      guild.members.kick(user);
+      interaction.guild.members.kick(user);
       await interaction.editReply(`✅ <@${user.id}> a été kick`);
     } catch (error) {
       console.error("[❌ERROR]", error);
