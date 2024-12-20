903f6ce30eb8ff4d94066234b7804ab8010bb919
Minilan <minilancraft@gmail.com>
2024-12-20 19:30:20 +0100

    Clear Command

--- commands/admins/clear.js
+++ commands/admins/clear.js
@@ -8,12 +8,23 @@ module.exports = {
     .setDescription("Supprimer un nombre spécifié de messages")
     .addIntegerOption((option) =>
       option
-        .setName("nombre")
+        .setName("number")
         .setDescription("Le nombre de messages à supprimer")
         .setRequired(true)
     ),
 
   // Execution
   async execute(interaction) {
+    const amount = interaction.options.getInteger("number");
+    if (amount < 1 || amount > 100) {
+      return interaction.reply(
+        "❌ Vous devez spécifier un nombre entre 1 et 100"
+      );
+    }
+    await interaction.channel.bulkDelete(amount, true);
+    await interaction.reply({
+      content: `✅ ${amount} messages ont été supprimés`,
+      ephemeral: true,
+    });
   },
 };
